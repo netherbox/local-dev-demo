61a0a70c1c80cae5c3276cf3d8894809fe30c2bd
Denis Zenkovich <netherbox@gmail.com>
2021-08-05 22:37:26 +0100

    Show the production status

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { environment } from 'src/environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'local-dev-demo';
+  isProduction: boolean;
+
+  constructor () {
+    this.isProduction = environment.production;
+  }
 }
